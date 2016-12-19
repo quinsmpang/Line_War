a3c5b0ec5151e271ca323fc143abc4c29026366f
Kradirhamik <ivocunha@outlook.com>
2016-12-19 20:56:28 +0100

    * Dynamic playername input to database

--- Assets/Resources/Scripts/Menu/DataInserter.cs
+++ Assets/Resources/Scripts/Menu/DataInserter.cs
@@ -1,18 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DataInserter : MonoBehaviour {
 	string CreatePlayerURL = "http://localhost/linewar/addplayer.php";
 
 	public string inputPlayertype;
-	public string inputPlayername;
+	public Text inputPlayername;
 	public string inputPlayerpassword;
+//	public Text inputPlayer;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Space)) {
@@ -21,10 +23,10 @@ public class DataInserter : MonoBehaviour {
 		}
 	}
 
-	public void CreateUser(string type, string playername, string password) {
+	public void CreateUser(string type, Text playername, string password) {
 		WWWForm form = new WWWForm ();
 		form.AddField ("post_player_type", type);
-		form.AddField ("post_player_name", playername);
+		form.AddField ("post_player_name", playername.text);
 		form.AddField ("post_player_password", password);
 
 		WWW www = new WWW (CreatePlayerURL, form);
