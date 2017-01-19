5e94bb007dfabeacb7da415fcf7923e58fe8e3c0
Kradirhamik <ivocunha@outlook.com>
2017-01-19 23:16:31 +0100

    + Method to add virtual currency for testing

---
+++ Assets/Resources/Scripts/Menu/Currency.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Currency : MonoBehaviour {
+	public Text coins;
+	private int _tempCoins;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddCoins() {
+		_tempCoins = int.Parse(coins.text);
+		_tempCoins += 10;
+		coins.text = _tempCoins.ToString();
+		print ("TRANSACTION: Current Coins: " + _tempCoins);
+	}
+}
