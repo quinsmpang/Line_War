0036655f7d1609c31f1f074f3d39bdf04ca72303
Kradirhamik <ivocunha@outlook.com>
2017-01-23 18:51:48 +0100

    + Player Skill picker

---
+++ Assets/Resources/Scripts/Menu/PlayerSkillPicker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerSkillPicker : MonoBehaviour {
+
+	public GameObject characterList; // Assign in inspector
+	public Button buttonPlayerSkill1;
+	public Button buttonPlayerSkill2;
+	private bool _isShowing;
+
+	void OnEnable() {
+		buttonPlayerSkill1.onClick.AddListener (ToggleCharactersList);
+		buttonPlayerSkill2.onClick.AddListener (ToggleCharactersList);
+	}
+
+	void ToggleCharactersList () {
+		_isShowing = !_isShowing;
+		characterList.SetActive(_isShowing);
+	}
+}
+
