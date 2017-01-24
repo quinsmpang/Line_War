52b74e3b46ba6943bb5296d9fc06058a3d274fc0
Kradirhamik <ivocunha@outlook.com>
2017-01-24 13:18:17 +0100

    + Character Picker

---
+++ Assets/Resources/Scripts/Menu/CharacterPicker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CharacterPicker : MonoBehaviour {
+
+	public GameObject characterList;
+	public Button buttonCharacter;
+	private bool _isShowing;
+
+	void OnEnable() {
+		buttonCharacter.onClick.AddListener (ToggleCharacterList);
+	}
+
+	void ToggleCharacterList () {
+		_isShowing = !_isShowing;
+		characterList.SetActive(_isShowing);
+	}
+}
+
