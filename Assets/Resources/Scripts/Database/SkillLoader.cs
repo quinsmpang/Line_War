6723ad37edd3603c5d3a19bef0ca027609fe4cf8
Kradirhamik <ivocunha@outlook.com>
2017-02-07 13:22:08 +0100

    * Merged playerSkills and characterSkills into Skills

---
+++ Assets/Resources/Scripts/Database/SkillLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillLoader : MonoBehaviour {
+	public string[] skillsList;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		WWW skillsData = new WWW("http://ivocunha.com/linewar/db_getskills.php");
+		yield return skillsData;
+		string skillsDataString = skillsData.text;
+		print ("PlayerSkillLoader: " + skillsDataString);
+		skillsList = skillsDataString.Split (';');
+		//print (GetDataValue(skillsList[1], "Name: "));
+	}
+
+	string GetDataValue(string data, string index) {
+		string value = data.Substring (data.IndexOf(index) + index.Length);
+		if (value.Contains("|")) {
+			value = value.Remove (value.IndexOf("|"));
+		}
+		return value;
+	}
+}
